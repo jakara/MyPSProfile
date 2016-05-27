1ac0be6a363b357778f8bb6b4cda3a2999bb38b3
付杰 <jiefu@tujia.com>
2016-05-27 18:13:10 +0800

    add Invoke-MSBuild

--- DotNetBuildTools/profile.ps1
+++ DotNetBuildTools/profile.ps1
@@ -1,2 +1,6 @@
 
+. (Join-Path $PSScriptRoot 'Invoke-MSBuild.ps1')
+
 . (Join-Path $PSScriptRoot 'Start-BuildingVSSolution.ps1')
+
+ Invoke-MsBuild -Path  "D:\Code\DotNet\Framework\Src\TuJia.Framework.sln" -MsBuildParameters "/target:Clean;Build /property:Configuration=Release;Platform=""Mixed Platforms""" 
